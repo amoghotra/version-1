3dfea6a6b6de519dd178ac92714796ce743eba36
amoghotra <noname@nati-iMac.local>
2016-07-11 18:08:20 +0100

    Still need to do some changes
    can sort of display: line-block

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>version1</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="img/">
+</head>
+
+<body>
+<div id="wrapper">
+    <div id="columns"> 
+          <ul class="img-list1">
+            <li>
+              <img src="img/homer.png">
+              <div class="text-content">
+              <p class="text_Content"><h2><span>About Us</h2></span></a>
+              <a href=""><span>hello</span></a><br>
+              <a href="http://www.twitter.com"><span>twitter</span></a><br>
+              <a href="http://www.facebook.com"><span>Facebook</span></a><br
+            </li></p></div>    
+          </ul>
+    </div>  
+     
+     <div id="columns"> 
+          <ul class="img-list1">
+            <li>
+              <img src="img/Blacklight-Skull.jpg">
+              <div class="text-content">
+              <p class="text_Content"><h2><span>About Us</h2></span></a>
+              <a href=""><span>hello</span></a><br>
+              <a href="http://www.twitter.com"><span>twitter</span></a><br>
+              <a href="http://www.facebook.com"><span>Facebook</span></a><br
+            </li></p></div>    
+          </ul>
+    </div> 
+
+  <div id="columns"> 
+          <ul class="img-list1">
+            <li>
+              <img src="img/hacked-story.jpg">
+              <div class="text-content">
+              <p class="text_Content"><h2><span>About Us</h2></span></a>
+              <a href=""><span>hello</span></a><br>
+              <a href="http://www.twitter.com"><span>twitter</span></a><br>
+              <a href="http://www.facebook.com"><span>Facebook</span></a><br
+            </li></p></div>    
+          </ul>
+    </div>  
+     
+     <div id="columns"> 
+          <ul class="img-list1">
+            <li>
+              <img src="img/android.png">
+              <div class="text-content">
+              <p class="text_Content"><h2><span>About Us</h2></span></a>
+              <a href=""><span>hello</span></a><br>
+              <a href="http://www.twitter.com"><span>twitter</span></a><br>
+              <a href="http://www.facebook.com"><span>Facebook</span></a><br
+            </li></p></div>    
+          </ul>
+    </div>
+
+    <div id="columns"> 
+          <ul class="img-list1">
+            <li>
+              <img src="img/icecat.svg">
+              <div class="text-content">
+              <p class="text_Content"><h2><span>About Us</h2></span></a>
+              <a href=""><span>hello</span></a><br>
+              <a href="http://www.twitter.com"><span>twitter</span></a><br>
+              <a href="http://www.facebook.com"><span>Facebook</span></a><br
+            </li></p></div>    
+          </ul>
+    </div>  
+     
+     <div id="columns"> 
+          <ul class="img-list1">
+            <li>
+              <img src="img/Blacklight-Skull.jpg">
+              <div class="text-content">
+              <p class="text_Content"><h2><span>About Us</h2></span></a>
+              <a href=""><span>hello</span></a><br>
+              <a href="http://www.twitter.com"><span>twitter</span></a><br>
+              <a href="http://www.facebook.com"><span>Facebook</span></a><br
+            </li></p></div>    
+          </ul>
+    </div>  
+        <div id="columns"> 
+          <ul class="img-list1">
+            <li>
+              <img src="img/twitterbrd.png">
+              <div class="text-content">
+              <p class="text_Content"><h2><span>About Us</h2></span></a>
+              <a href=""><span>hello</span></a><br>
+              <a href="http://www.twitter.com"><span>twitter</span></a><br>
+              <a href="http://www.facebook.com"><span>Facebook</span></a><br
+            </li></p></div>    
+          </ul>
+    </div>  
+     
+     
+     
+
+</body>
+</html>
